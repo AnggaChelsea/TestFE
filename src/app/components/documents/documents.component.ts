9b730c11f24439740c3e485785e9dda20baa517c
AnggaChelsea <muhidabdul168@gmail.com>
2021-01-30 22:49:18 +0700

    bisa

--- src/app/components/documents/documents.component.ts
+++ src/app/components/documents/documents.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { VideoService } from '../../services/video.service'
 @Component({
   selector: 'app-documents',
   templateUrl: './documents.component.html',
   styleUrls: ['./documents.component.css']
 })
 export class DocumentsComponent implements OnInit {
+  datavideo:any;
 
-  constructor() { }
+  constructor(private videoService:VideoService) { }
 
   ngOnInit(): void {
+    this.getvideo();
+  }
+
+  getvideo(){
+    return this.videoService.readAll().subscribe(data => {
+      this.datavideo = data.list;
+      console.log(this.datavideo);
+    })
   }
 
 }
--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -13,43 +13,10 @@ export class MainComponent implements OnInit {
 
   cols!: number;
 
-  gridByBreakpoint = {
-    xl: 3,
-    lg: 3,
-    md: 3,
-    sm: 2,
-    xs: 1
-  }
-
   datavideo: any;
   constructor(private videoService:VideoService, private breakpointObserver: BreakpointObserver) 
   { 
     this.showNotification = true;
-    // this.breakpointObserver.observe([
-    //   Breakpoints.XSmall,
-    //   Breakpoints.Small,
-    //   Breakpoints.Medium,
-    //   Breakpoints.Large,
-    //   Breakpoints.XLarge,
-    // ]).subscribe(result => {
-    //   if (result.matches) {
-    //     if (result.breakpoints[Breakpoints.XSmall]) {
-    //       this.cols = this.gridByBreakpoint.xs;
-    //     }
-    //     if (result.breakpoints[Breakpoints.Small]) {
-    //       this.cols = this.gridByBreakpoint.sm;
-    //     }
-    //     if (result.breakpoints[Breakpoints.Medium]) {
-    //       this.cols = this.gridByBreakpoint.md;
-    //     }
-    //     if (result.breakpoints[Breakpoints.Large]) {
-    //       this.cols = this.gridByBreakpoint.lg;
-    //     }
-    //     if (result.breakpoints[Breakpoints.XLarge]) {
-    //       this.cols = this.gridByBreakpoint.xl;
-    //     }
-    //   }
-    // });
   }
 
   ngOnInit(): void {
