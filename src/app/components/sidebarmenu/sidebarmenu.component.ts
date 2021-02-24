63fcef9844cfb437a64555701f3d98f567bb2afb
AnggaChelsea <muhidabdul168@gmail.com>
2021-02-24 10:29:18 +0700

    .

--- src/app/components/sidebarmenu/sidebarmenu.component.ts
+++ src/app/components/sidebarmenu/sidebarmenu.component.ts
@@ -4,8 +4,10 @@ interface Item {
   name: string;
   image: string;
   viwers: number;
+  logo:string;
   addedToCart: boolean;
   desc: string;
+  time:string;
 }
 
 @Component({
@@ -20,6 +22,8 @@ export class SidebarmenuComponent implements OnInit {
       image: 'https://linisehat.com/wp-content/uploads/2020/12/hiking.jpg',
       viwers: 444,
       addedToCart: false,
+      logo:`fa fa-video-camera`,
+      time:'44 minutes ago',
       desc: `
         Demon sword that harms and effectively kills oddities.
         The sword belonged to a powerful  Kiss-Shot Acerola-Orion Heart-Under-Blade.`
@@ -29,6 +33,8 @@ export class SidebarmenuComponent implements OnInit {
       image: 'https://lifeexperiencedegreepros.com/wp-content/uploads/2019/01/Best-Holidays-For-Families-in-Africa.jpg',
       viwers: 555,
       addedToCart: false,
+      logo:'fa fa-lock',
+      time:'44 minutes ago',
       desc: `Star Platinum is the Stand of Kujo Jotaro.
       It has long, flowing hair, and resembling a tall, well-built man.
       It is silent, except when it throws punches, during which it cries "ORAORAORA" loudly and repeatedly.`
@@ -38,6 +44,8 @@ export class SidebarmenuComponent implements OnInit {
       name: '3D Maneuver Gear',
       viwers: 200,
       addedToCart: false,
+      logo:'fa fa-comment',
+      time:'44 minutes ago',
       image: 'https://www.incimages.com/uploaded_files/image/1920x1080/getty_1141196125_411306.jpg',
       desc: `The vertical maneuvering equipment is a set of equipment developed by humans allowing for great mobility.
       The equipment enables the user to fight in a 3D space as opposed to a 2D one.
@@ -47,6 +55,8 @@ export class SidebarmenuComponent implements OnInit {
       name: 'Excalibur',
       viwers: 300,
       addedToCart: false,
+      logo:'fa fa fa-heart',
+      time:'44 minutes ago',
       image: 'https://1.bp.blogspot.com/-GLyYqnoJ0aA/XoGkI0EhbiI/AAAAAAAA-zk/3A7Ouql_9Eg80k_us7gaDo6HiVoc0xE7QCLcBGAsYHQ/s640/persiapan%2Bmendaki%2Bgunung.jpg',
       desc: `Excalibur: Sword of Promised Victory is the strongest and most majestic holy sword that symbolizes King Arthur.
       As that which can be called the physical actualization of her ideals and the symbol of her heroism,
@@ -56,6 +66,8 @@ export class SidebarmenuComponent implements OnInit {
       name: 'Dragon Slayer',
       viwers: 450,
       addedToCart: false,
+      logo:'fa fa-video-camera',
+      time:'44 minutes ago',
       image: 'http://indonesia360derajat.files.wordpress.com/2014/04/satu.jpg?w=783&h=586',
       desc: `It was too big to be called a sword. Massive, thick, heavy, and far too rough.
       Indeed, it was a heap of raw iron.
@@ -65,6 +77,8 @@ export class SidebarmenuComponent implements OnInit {
       name: 'Dragon Slayer',
       viwers: 450,
       addedToCart: false,
+      logo:'fa fa-video-camera',
+      time:'44 minutes ago',
       image: 'http://indonesia360derajat.files.wordpress.com/2014/04/satu.jpg?w=783&h=586',
       desc: `It was too big to be called a sword. Massive, thick, heavy, and far too rough.
       Indeed, it was a heap of raw iron.
@@ -74,6 +88,8 @@ export class SidebarmenuComponent implements OnInit {
       name: 'Dragon Slayer',
       viwers: 450,
       addedToCart: false,
+      logo:'fa fa-video-camera',
+      time:'44 minutes ago',
       image: 'http://indonesia360derajat.files.wordpress.com/2014/04/satu.jpg?w=783&h=586',
       desc: `It was too big to be called a sword. Massive, thick, heavy, and far too rough.
       Indeed, it was a heap of raw iron.
@@ -83,6 +99,8 @@ export class SidebarmenuComponent implements OnInit {
       name: 'Dragon Slayer',
       viwers: 450,
       addedToCart: false,
+      logo:'fa fa-love',
+      time:'44 minutes ago',
       image: 'http://indonesia360derajat.files.wordpress.com/2014/04/satu.jpg?w=783&h=586',
       desc: `It was too big to be called a sword. Massive, thick, heavy, and far too rough.
       Indeed, it was a heap of raw iron.
@@ -92,6 +110,8 @@ export class SidebarmenuComponent implements OnInit {
       name: 'Dragon Slayer',
       viwers: 450,
       addedToCart: false,
+      logo:'',
+      time:'44 minutes ago',
       image: 'http://indonesia360derajat.files.wordpress.com/2014/04/satu.jpg?w=783&h=586',
       desc: `It was too big to be called a sword. Massive, thick, heavy, and far too rough.
       Indeed, it was a heap of raw iron.
@@ -101,6 +121,8 @@ export class SidebarmenuComponent implements OnInit {
       name: 'Dragon Slayer',
       viwers: 450,
       addedToCart: false,
+      logo:'',
+      time:'44 minutes ago',
       image: 'http://indonesia360derajat.files.wordpress.com/2014/04/satu.jpg?w=783&h=586',
       desc: `It was too big to be called a sword. Massive, thick, heavy, and far too rough.
       Indeed, it was a heap of raw iron.
@@ -110,6 +132,8 @@ export class SidebarmenuComponent implements OnInit {
       name: 'Dragon Slayer',
       viwers: 450,
       addedToCart: false,
+      logo:'',
+      time:'44 minutes ago',
       image: 'http://indonesia360derajat.files.wordpress.com/2014/04/satu.jpg?w=783&h=586',
       desc: `It was too big to be called a sword. Massive, thick, heavy, and far too rough.
       Indeed, it was a heap of raw iron.
@@ -119,6 +143,8 @@ export class SidebarmenuComponent implements OnInit {
       name: 'Dragon Slayer',
       viwers: 450,
       addedToCart: false,
+      logo:'',
+      time:'44 minutes ago',
       image: 'http://indonesia360derajat.files.wordpress.com/2014/04/satu.jpg?w=783&h=586',
       desc: `It was too big to be called a sword. Massive, thick, heavy, and far too rough.
       Indeed, it was a heap of raw iron.
@@ -128,6 +154,8 @@ export class SidebarmenuComponent implements OnInit {
       name: 'Dragon Slayer',
       viwers: 450,
       addedToCart: false,
+      logo:'',
+      time:'44 minutes ago',
       image: 'http://indonesia360derajat.files.wordpress.com/2014/04/satu.jpg?w=783&h=586',
       desc: `It was too big to be called a sword. Massive, thick, heavy, and far too rough.
       Indeed, it was a heap of raw iron.
@@ -137,6 +165,8 @@ export class SidebarmenuComponent implements OnInit {
       name: 'Dragon Slayer',
       viwers: 450,
       addedToCart: false,
+      logo:'',
+      time:'44 minutes ago',
       image: 'http://indonesia360derajat.files.wordpress.com/2014/04/satu.jpg?w=783&h=586',
       desc: `It was too big to be called a sword. Massive, thick, heavy, and far too rough.
       Indeed, it was a heap of raw iron.
@@ -146,6 +176,8 @@ export class SidebarmenuComponent implements OnInit {
       name: 'Dragon Slayer',
       viwers: 450,
       addedToCart: false,
+      logo:'',
+      time:'44 minutes ago',
       image: 'http://indonesia360derajat.files.wordpress.com/2014/04/satu.jpg?w=783&h=586',
       desc: `It was too big to be called a sword. Massive, thick, heavy, and far too rough.
       Indeed, it was a heap of raw iron.
@@ -155,6 +187,8 @@ export class SidebarmenuComponent implements OnInit {
       name: 'Dragon Slayer',
       viwers: 450,
       addedToCart: false,
+      logo:'',
+      time:'44 minutes ago',
       image: 'http://indonesia360derajat.files.wordpress.com/2014/04/satu.jpg?w=783&h=586',
       desc: `It was too big to be called a sword. Massive, thick, heavy, and far too rough.
       Indeed, it was a heap of raw iron.
@@ -162,9 +196,15 @@ export class SidebarmenuComponent implements OnInit {
     },
   ];
 
+  buttonClose:boolean = true;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  Close(){
+    this.buttonClose = false
   }
 
 }
